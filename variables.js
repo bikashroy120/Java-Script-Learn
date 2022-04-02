d813eddcac38c95ab92e0895b55d9de5dd222bac
bikashroy120 <91220054+bikashroy120@users.noreply.github.com>
2022-04-02 16:21:05 +0600

    first commit

---
+++ variables.js
@@ -0,0 +1,41 @@
+// 4 Ways to Declare a JavaScript Variable:
+
+// Using var
+// Using let
+// Using const
+// Using nothing
+
+
+
+// Variables are containers for storing data (storing data values).
+
+// In this example, x, y, and z, are variables, declared with the var keyword:
+
+
+// var x = 5;
+// var y = 6;
+// var z = x + y;
+
+// console.log(z);
+
+
+// In this example, x, y, and z, are variables, declared with the let keyword:
+
+// let x = 5;
+// let y = 6;
+// let z = x + y;
+
+// console.log(z);
+
+
+
+
+// If you want a general rule: always declare variables with const.
+// If you think the value of the variable can change, use let.
+// In this example, price1, price2, and total, are variables:
+
+
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+console.log(total);
